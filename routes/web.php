cc759a8e7833dde2b09853fd8112511d8493ad18
yashborda7048 <yashborda123@gmail.com>
2023-06-02 20:29:35 +0530

    add migrate

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Usercontroller;
+use App\Http\Controllers\StudentController;
 
-Route::view('/', 'home')->name('home');
+Route::view('/', [StudentController::class, 'index'])->name('home');
 Route::view('/add', 'add')->name('add');
 Route::view('/update', 'update')->name('update');
 Route::view('/delete', 'delete')->name('delete');
 Route::get('/about/{user?}', [Usercontroller::class, 'userName'])->name('about');
-
-// Route::redirect('/about', 'test', 301);
-
-// Route::prefix('texts')->group(function () {
-
-//     Route::get('/post-1', function () {
-//         return view('tests/post-1');F
-//     });
-//     Route::view('/post-2', 'tests/post-2');
-//     Route::view('/post-3', 'tests/post-3');
-// });
 
 Route::fallback(function () {
     return "<h1>Page Not Found.</h1>";
